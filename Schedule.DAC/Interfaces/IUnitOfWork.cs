08fb3ec5fba8ef776b1ea1dc02445ef14e94498e
Nikita <nbandaruk@gmail.com>
2017-09-02 12:45:14 +0300

    Added classes and interfaces for ASP.NET Identity.

---
+++ Schedule.DAC/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedule.DAL.Identity;
+using System.Threading.Tasks;
+
+namespace Schedule.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ApplicationUserManager UserManager { get; }
+        IClientManager ClientManager { get; }
+        ApplicationRoleManager RoleManager { get; }
+        Task SaveAsync();
+    }
+}
---
+++ Schedule.DAC/Repositories/ClientManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedule.DAL.EF;
+using Schedule.DAL.Entities;
+using Schedule.DAL.Interfaces;
+
+namespace Schedule.DAL.Repositories
+{
+    public class ClientManager : IClientManager
+    {
+        public ApplicationContext Database { get; set; }
+        public ClientManager (ApplicationContext db)
+        {
+            Database = db;
+        }
+
+        public void Create(ClientProfile item)
+        {
+            Database.ClientProfiles.Add(item);
+            Database.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
