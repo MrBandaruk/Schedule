452a75d5ab1314ef8fe5c925053f0143b2ee0476
Nikita <nbandaruk@gmail.com>
2017-09-02 12:31:37 +0300

    Added some classes and folders for ASP.NET Identity. Changed names.

---
+++ Schedule.DAC/Identity/ApplicationUserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Schedule.DAL.Entities;
+
+namespace Schedule.DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+                : base(store)
+        {
+        }
+    }
+}
