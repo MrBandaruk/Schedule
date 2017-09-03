22f9e2f02d1a4e01151c086ab8c8d18d1d7ca54d
Nikita <nbandaruk@gmail.com>
2017-09-03 17:21:35 +0300

    Added interfaces and classes for ASP.NET Identity.

---
+++ Schedule.BLL/Services/ServiceCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedule.BLL.Interfaces;
+using Schedule.DAL.Repositories;
+
+namespace Schedule.BLL.Services
+{
+    public class ServiceCreator : IServiceCreator
+    {
+        public IUserService CreateUserService(string connection)
+        {
+            return new UserService(new IdentityUnitOfWork(connection));
+        }
+    }
+}
