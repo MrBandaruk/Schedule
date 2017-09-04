9c9a0876aedd4f944f97756c29b4caeab6a72fdd
Nikita <nbandaruk@gmail.com>
2017-09-04 14:15:01 +0300

    Added ASP.NET Identity without views.

---
+++ Schedule/App_Start/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Schedule.BLL.Services;
+using Microsoft.AspNet.Identity;
+using Schedule.BLL.Interfaces;
+
+
+[assembly: OwinStartup(typeof(Schedule.App_Start.Startup))]
+
+
+namespace Schedule.App_Start
+{
+    public class Startup
+    {
+        IServiceCreator serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IUserService>(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return serviceCreator.CreateUserService("DefaultConnection");
+        }
+    }
+}
