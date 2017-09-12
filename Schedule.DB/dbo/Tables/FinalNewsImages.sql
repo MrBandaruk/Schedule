4645074320e3477e3d75b849a07a45276018efa7
Nikita <nbandaruk@gmail.com>
2017-09-12 23:52:52 +0300

    Updating names

---
+++ Schedule.DB/dbo/Tables/FinalNewsImages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FinalNewsImages] (
+    [Id]        INT             IDENTITY (1, 1) NOT NULL,
+    [ImageItem] VARBINARY (MAX) NOT NULL,
+    [NewsId]    INT             NOT NULL,
+    CONSTRAINT [PK_FinalNewsImages] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_FinalNewsImages_FinalNews] FOREIGN KEY ([NewsId]) REFERENCES [dbo].[FinalNews] ([Id])
+);
+
