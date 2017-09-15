0a50613ccd9db3cf9395e4557fc58871fbd816c1
Nikita <nbandaruk@gmail.com>
2017-09-15 08:59:38 +0300

    added ErrorController and views. TODO: fix errors without controllers.

---
+++ Schedule/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Schedule.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult General(Exception exception)
+        {
+            return View("Exception", exception);
+        }
+
+        public ActionResult Http404()
+        {
+            return View();
+        }
+
+        public ActionResult Http403()
+        {
+            return View();
+        }
+
+        //public ActionResult Exception()
+        //{
+        //    return View();
+        //}
+    }
+}
--- Schedule/Global.asax.cs
+++ Schedule/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Schedule.Controllers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,6 +19,40 @@ namespace Schedule
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/Web.config")));
+        }
+
+        protected void Application_Error()
+        {
+            
+            var exception = Server.GetLastError();
+            var httpException = exception as HttpException;
+            Response.Clear();
+            Server.ClearError();
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = "General";
+            //routeData.Values["exception"] = exception;
+            Response.StatusCode = 500;
+
+            if (httpException != null) {
+            Response.StatusCode = httpException.GetHttpCode();
+
+                switch (Response.StatusCode) {
+            case 403:
+                routeData.Values["action"] = "Http403";
+                break;
+            case 404:
+                routeData.Values["action"] = "Http404";
+                break;
+            }
+
+            }
+            Response.TrySkipIisCustomErrors = true;
+            IController errorsController = new ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
+            
         }
     }
 }
