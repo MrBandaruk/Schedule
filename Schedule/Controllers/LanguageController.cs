a4f62d95e95ebe7d8e824d8497c11f2cff2d868c
Nikita <nbandaruk@gmail.com>
2017-09-27 16:38:01 +0300

    Trying to make correct redirect after changing lang.

--- Schedule/Controllers/LanguageController.cs
+++ Schedule/Controllers/LanguageController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Schedule.Controllers
 {
@@ -18,15 +19,23 @@ namespace Schedule.Controllers
 
         public ActionResult Change(string lang)
         {
+            //set lang
             if(lang != null)
             {
                 Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(lang);
                 Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
             }
 
+            //save lang to cookies
             HttpCookie cookie = new HttpCookie("Language");
             cookie.Value = lang;
             Response.Cookies.Add(cookie);
+
+            //get action and controller for redirecting back to the same page
+            //var currentController = this.RouteData.Values["controller"];
+            //var currentAction = this.RouteData.Values["action"];
+            //return RedirectToAction(currentAction.ToString(), currentController.ToString());
+
             return RedirectToAction("Index", "Home");
         }
     }
