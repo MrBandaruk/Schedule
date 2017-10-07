ff89bdd42b0808bed6d8c09d30407dab3f5012fd
Nikita <nbandaruk@gmail.com>
2017-10-07 23:52:35 +0300

    Added textbox chars couters to Create and Update.

---
+++ Schedule/Scripts/Custom/CharsLeft.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+	var shortTitleChars = $(".char:eq( 0 )");
+	var fullTitleChars = $(".char:eq( 1 )");
+	var shortArticleChars = $(".char:eq( 2 )");
+
+	shortTitleChars.text(60 - $("#tdShortTitle").val().length + " left");
+	fullTitleChars.text(256 - $("#tdFullTitle").val().length + " left");
+	shortArticleChars.text(410 - $("#tdShortArticle").val().length + " left"); 
+
+	Counter(shortTitleChars, 60, $("#tdShortTitle"));
+	Counter(fullTitleChars, 256, $("#tdFullTitle"))
+	Counter(shortArticleChars, 410, $("#tdShortArticle"))
+});
+
+function Counter(chars, maxChars, input) {
+	$(input).on("keydown keyup focus keypress", function () {
+		var len = $(this).val().length;
+		len = maxChars - len;
+		if (!len) {
+			$(this).val($(this).val().substr(0, maxChars - 1));
+			
+			$(chars).css('color', 'red').text("You have reached the maximum number of characters.");
+		} else {
+			$(chars).css('color', 'gray').text(len + " left");
+		}
+		
+	});
+}
